821bf5b4051709265633332db5b9796af23160bb
afbelardi <afbelardi@gmail.com>
2023-07-21 19:01:53 -0400

    first collection products showing

---
+++ assets/collection-list.js
@@ -0,0 +1,17 @@
+class CollectionTabs extends HTMLElement {
+    constructor() {
+      super();
+  
+      onClick(console.log(this.querySelectorAll('[data-url]')))
+
+     
+    
+    }     
+    
+  }  
+    
+
+    
+  
+  
+  customElements.define('collection-tabs', CollectionTabs);
