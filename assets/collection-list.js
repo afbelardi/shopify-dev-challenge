5f1421c3ccb05e69ba1d6dd0eb1f1b2c5abac0e9
afbelardi <afbelardi@gmail.com>
2023-07-22 11:43:44 -0400

    getting products from specific collections to the store

--- assets/collection-list.js
+++ assets/collection-list.js
@@ -9,6 +9,41 @@ class CollectionTabs extends HTMLElement {
 
         const firstTitle = firstTab.querySelector('.collection_titles');
         firstTitle.classList.add('active');
+
+
+        desktopTabs.forEach((tab) => {
+          tab.addEventListener('click', () => {
+            const currentActiveTab = this.querySelector('.tabs-desktop li.active');
+            if (currentActiveTab) {
+              currentActiveTab.classList.remove('active');
+
+              const currentActiveTitle = currentActiveTab.querySelector('.collection_titles');
+              if (currentActiveTitle) {
+                currentActiveTitle.classList.remove('active');
+              }
+            }
+
+            tab.classList.add('active');
+            const clickedTitle = tab.querySelector('.collection_titles');
+            if (clickedTitle) {
+              clickedTitle.classList.add('active');
+            }
+
+
+            const collectionId = tab.dataset.collectionId;
+            console.log(collectionId);
+
+            fetchProductsFromCollection(collectionId)
+              .then((response) => response.json())
+              .then((data) => {
+                console.log(data.products);
+              })
+              .catch((error) => {
+                console.error(error);
+              })
+
+          })
+        })
        
       })
     }
@@ -20,3 +55,9 @@ class CollectionTabs extends HTMLElement {
   
   
   customElements.define('collection-tabs', CollectionTabs);
+
+
+  function fetchProductsFromCollection(collectionId) {
+    console.log('worked');
+    return fetch(`/collections/${collectionId}/products.json`);
+  }
