bcee0ad644010e918391df46439d3748e5e80e81
afbelardi <afbelardi@gmail.com>
2023-07-23 12:32:12 -0400

    retrieved section showing in the console

--- assets/collection-list.js
+++ assets/collection-list.js
@@ -10,60 +10,130 @@ class CollectionTabs extends HTMLElement {
         const firstTitle = firstTab.querySelector('.collection_titles');
         firstTitle.classList.add('active');
 
-
         desktopTabs.forEach((tab) => {
           tab.addEventListener('click', () => {
             const currentActiveTab = this.querySelector('.tabs-desktop li.active');
             if (currentActiveTab) {
               currentActiveTab.classList.remove('active');
-
+  
               const currentActiveTitle = currentActiveTab.querySelector('.collection_titles');
               if (currentActiveTitle) {
                 currentActiveTitle.classList.remove('active');
               }
             }
-
+  
             tab.classList.add('active');
             const clickedTitle = tab.querySelector('.collection_titles');
             if (clickedTitle) {
               clickedTitle.classList.add('active');
             }
-
-
-            const collectionTitle = tab.dataset.collectionTitle;
-            console.log(collectionTitle);
-
-
-            fetchProductsFromCollection(collectionTitle)
-              .then((response) => response.text())
-              .then((htmlContent) => {
-                const parser = new DOMParser();
-                const doc = parser.parseFromString(htmlContent, 'text/html');
-                const products = doc.querySelectorAll('.card');
-                console.log(products);
-              })
-              .catch((error) => {
-                console.error(error)
-              })
-
-          
-
+  
           })
         })
-       
       })
+
+
+      
+
+      this.querySelectorAll("[data-collection-url]").forEach( tab => { 
+        tab.addEventListener('click', (e) => { 
+         fetchSection(tab.dataset.collectionUrl)
+        }) 
+      })
+
+     async function fetchSection (collectionUrl) {
+        const response = await fetch(collectionUrl + "/?sections=retrieved-section");
+  
+        const data = await response.text()
+        const sections = JSON.parse(data)
+        console.log(sections);
+        const thisDocument = new DOMParser().parseFromString(sections['retrieved-section'], 'text/html');
+        console.log(thisDocument);
+        // this.appendChild(thisDocument.getElementsByTagName('body'));
+
+        const newUrl = `${window.location.pathname}${collectionUrl}?sections=retrieved-section`;
+        history.pushState({}, '', newUrl);
+      }
+     
     }
+
+
+    
     }     
     
     
-
-    
   
+    
+ 
   
   customElements.define('collection-tabs', CollectionTabs);
 
 
-  function fetchProductsFromCollection(collectionTitle) {
-    console.log('worked');
-    return fetch(`/collections/${collectionTitle}`);
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // function fetchProductsFromCollection(collectionTitle) {
+  //   console.log('worked');
+  //   return fetch(`/collections/${collectionTitle}`);
+  // }
+
+ 
+
+  // function fetchProductsAndRenderToSection(collectionUrl, sectionId) {
+  //   console.log(sectionId);
+  //   const sectionId = 'shopify-section-collection-products';
+  //   const url = `${collectionUrl}?sections=${sectionId}`;
+
+  // return fetch(url)
+  //   .then((response) => {
+  //     if (!response.ok) {
+  //       throw new Error('Network response error');
+  //     }
+  //     return response.text();
+  //   })
+  //   .then((htmlContent) => {
+  //     const sectionContainer = document.getElementById(sectionId);
+  //     if (sectionContainer) {
+  //       sectionContainer.innerHTML = htmlContent;
+  //     }
+
+
+  //   })
+  //   .catch((error) => {
+  //     console.error(error);
+  //   });
+  // }
+
+    // fetchProductsFromCollection(collectionTitle)
+            //   .then((response) => response.text())
+            //   .then((htmlContent) => {
+            //     const parser = new DOMParser();
+            //     const doc = parser.parseFromString(htmlContent, 'text/html');
+            //     const products = doc.querySelectorAll('.card');
+            //     console.log(products);
+            //   })
+            //   .catch((error) => {
+            //     console.error(error)
+            //   })
+
+          
